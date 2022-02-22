c96fff19790dd67cb4a4700f54f804253ba136a8
Sini <siniesofia@protonmail.com>
2022-02-22 16:41:30 +0200

    viikko2tehtävä3

--- src/varasto.py
+++ src/varasto.py
@@ -5,9 +5,9 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
+
         if alku_saldo < 0.0:
-            # virheellinen, nollataan 
+            # virheellinen, nollataan
             self.saldo = 0.0
         elif alku_saldo <= tilavuus:
             # mahtuu
@@ -20,6 +20,7 @@ class Varasto:
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
+
     def lisaa_varastoon(self, maara):
         if maara < 0:
             return
@@ -31,7 +32,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +41,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
